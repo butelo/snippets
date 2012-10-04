462c9a3fb32d0be30dc70f1f7829030a692a466f
iria <iria@192.168.1.100>
2012-10-04 17:43:19 +0200

    asdf

--- Snippets/src/com/muaki/snippets/Snippets.java
+++ Snippets/src/com/muaki/snippets/Snippets.java
@@ -2,19 +2,51 @@ package com.muaki.snippets;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Snippets extends Activity {
+public class Snippets extends ListActivity {
+	
+	 String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+	            "KeyTest", "AccelerometerTest", "AssetsTest",
+	            "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
+	            "FullScreenTest", "WakeLockTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+	            "FontTest", "SurfaceViewTest" };
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_snippets);
+//        setContentView(R.layout.activity_snippets);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tests));
     }
 
-    @Override
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_snippets, menu);
         return true;
+    }
+	
+	
+    @Override
+    protected void onListItemClick(ListView list, View view, int position,
+            long id) {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try {
+            Class clazz = Class
+                    .forName("com.muaki.snippets." + testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
